138787b1d54130609a58af7162e43a9b011a9f74
calanuzao <calanuzao@gmail.com>
2025-03-10 14:31:54 -0400

    updated theme & ascii art

--- init.lua
+++ init.lua
@@ -27,11 +27,101 @@ require('calanuzao.options')
 
 -- Theme Picker (themery.nvim)
 require("themery").setup({
-  themes = {"catppuccin","one","gruvbox","tokyonight"}, 
+  themes = {"catppuccin","one","gruvbox","tokyonight","cyberdream", "rose-pine-main", "rose-pine-moon", "rose-pine-dawn"}, 
   livePreview = true, 
 })
 
--- Themes
+-- THEMES
+-- ROSE-PINE THEME
+require("rose-pine").setup({
+  variant = "dawn", -- auto, main, moon, or dawn
+  dark_variant = "dawn", -- main, moon, or dawn
+  dim_inactive_windows = false,
+  extend_background_behind_borders = true,
+
+  enable = {
+      terminal = true,
+      legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+      migrations = true, -- Handle deprecated options automatically
+  },
+
+  styles = {
+      bold = true,
+      italic = true,
+      transparency = false,
+  },
+
+  groups = {
+      border = "muted",
+      link = "iris",
+      panel = "surface",
+
+      error = "love",
+      hint = "iris",
+      info = "foam",
+      note = "pine",
+      todo = "rose",
+      warn = "gold",
+
+      git_add = "foam",
+      git_change = "rose",
+      git_delete = "love",
+      git_dirty = "rose",
+      git_ignore = "muted",
+      git_merge = "iris",
+      git_rename = "pine",
+      git_stage = "iris",
+      git_text = "rose",
+      git_untracked = "subtle",
+
+      h1 = "iris",
+      h2 = "foam",
+      h3 = "rose",
+      h4 = "gold",
+      h5 = "pine",
+      h6 = "foam",
+  },
+
+  palette = {
+      -- Override the builtin palette per variant
+      -- moon = {
+      --     base = '#18191a',
+      --     overlay = '#363738',
+      -- },
+  },
+
+-- NOTE: Highlight groups are extended (merged) by default. Disable this
+-- per group via `inherit = false`
+  highlight_groups = {
+      -- Comment = { fg = "foam" },
+      -- StatusLine = { fg = "love", bg = "love", blend = 15 },
+      -- VertSplit = { fg = "muted", bg = "muted" },
+      -- Visual = { fg = "base", bg = "text", inherit = false },
+  },
+
+  before_highlight = function(group, highlight, palette)
+      -- Disable all undercurls
+      -- if highlight.undercurl then
+      --     highlight.undercurl = false
+      -- end
+      --
+      -- Change palette colour
+      -- if highlight.fg == palette.pine then
+      --     highlight.fg = palette.foam
+      -- end
+  end,
+})
+
+vim.cmd("colorscheme rose-pine")
+-- vim.cmd("colorscheme rose-pine-main")
+-- vim.cmd("colorscheme rose-pine-moon")
+-- vim.cmd("colorscheme rose-pine-dawn")
+
+-- LIGHT THEME
+-- require("cyberdream").load("light") -- Select This to fit Vscode Setup
+-- vim.cmd("colorscheme cyberdream")   -- Select This to fit Vscode Setup
+
+-- OTHER THEMES
 -- vim.o.background = "light" (I don't remember what this does)
 -- vim.cmd("colorscheme one")
 -- vim.cmd([[colorscheme gruvbox]])
@@ -103,3 +193,10 @@ vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, s
 -- Tab navigation using Ctrl+j and Ctrl+k
 vim.api.nvim_set_keymap('n', '<C-j>', ':tabnext<CR>', { noremap = true, silent = true })     -- Next tab
 vim.api.nvim_set_keymap('n', '<C-k>', ':tabprevious<CR>', { noremap = true, silent = true }) -- Previous tab
+
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
